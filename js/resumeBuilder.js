07f043fb5c6b9db55c12561a12e41f8c0f935ed7
Irene Canuti <irene.c@turnrealityon.com>
2015-12-18 17:54:30 +0100

    Objects Education and Bio added to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,62 @@
+var bio = {
+	"name" : "Irene Canuti",
+	"role" : "Digital Designer and PM",
+	"contacts" : {
+		"mobile" : "+44(0)7721140857"
+		"email" : "irene.c@turnrealityon.com",
+		"github" : "nenoch"
+		"twitter" : "@irenecanuti_tro"
+		"location" : "London, UK"
+	},
+
+	"welcomeMessage" : "Welcome to my resume!",
+	"skills" : ["Visual Identity", "HTML/CSS", "Wordpress", "Adobe CS: Photoshop, InDesign, Illustrator", "Information Design", "Design Thinking", "Digital Strategy", "Brand Management", "Product Management", "Team building",
+"Lean Startup"]
+};
+
+var education = {
+"schools" : [
+	{
+	"name" : "Kingston University",
+	"dates" : "2014-2016",
+	"location" : "Kingston Upon Thames, Greater London",
+	"degree" : "MA",
+	"majors" : ["Communication Design", "Human Centred Design", "Creative Economy"],
+	"url" : "http://www.kingston.ac.uk/"
+},
+{
+	"name" : "Universita degli Studi di Siena",
+	"dates" : "2005-2010",
+	"location" : "Siena, Italy",
+	"degree" : "BA",
+	"majors" : ["Communication Sciences", "Mass Communication", "New Media"],
+	"url" : "http://en.unisi.it/"
+}
+
+],
+
+"onlineCourses" : [
+	{
+	"school" : "Udacity",
+	"dates" : "2015 - Present",
+	"title" : "FEND : Front-End Web Developer",
+	"url" : "https://www.udacity.com"
+},
+	{
+	"school" : "Coder Manual",
+	"dates" : "2015",
+	"title" : "Web Developer Bootcamp",
+	"url" : "https://codermanual.com/"
+},
+	{
+	"school" : "General Assembly London",
+	"dates" : "February 2015",
+	"city" : "London, UK",
+	"title" : "UX Design Bootcamp",
+	"url" : "https://generalassemb.ly/"
+}
+]
+
+}
+
+
